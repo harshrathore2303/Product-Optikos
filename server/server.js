ad6d9416f42a5aeb92271e4c241b70288e4bd04b
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-11-26 15:19:22 +0530

    changes to cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://lgan9k1jnh.execute-api.ap-south-1.amazonaws.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
